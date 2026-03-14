b617f3dfc10de3942eca6959720971bdec52237c
Arthur Ruiz <23.01445-8@maua.br>
2026-03-14 09:32:32 -0300

    learn(ap01-js): lida com o código 3.3.1

--- intro_js/index.js
+++ intro_js/index.js
@@ -198,13 +198,143 @@
 // console.log(pessoa.nome)
 // console.log(pessoa['idade'])
 //Uma pessoa se chama Maria, tem 21 anos e mora na rua B, número 50
-const pessoa = {
-    nome: 'Maria',
-    idade: 21,
-    endereco:{
-        rua: 'Rua B',
-        numero: 50
-    }
+// const pessoa = {
+//     nome: 'Maria',
+//     idade: 21,
+//     endereco:{
+//         rua: 'Rua B',
+//         numero: 50
+//     }
+// }
+// console.log(pessoa.endereco.rua)
+// console.log(pessoa['endereco']['numero'])
+
+//Uma concessionária tem nome, cnpj e endereço (logradouro, número e bairro). Ela também tem seu estoque de veículos. A quantidade de veículos é arbitrária. A qualquer instante, ela pode ter 2 ou 5 ou 17 veículos. Cada veículo modelo, marca e placa.
+// const concessionária = {
+//     nome: 'Concessionária A',
+//     cnpj: '12345678', 
+//     endereco:{
+//         logradouro: 'Rua A',
+//         numero: '123', 
+//         bairro: {
+//             nome: 'Tatuapé',
+//         }
+//     },
+//     estoque:[
+//         {
+//             modelo: 'Corsa',
+//             marca: 'Chevrolet', 
+//             placa: 'DDF-3578'
+//         },
+//         {
+//             modelo: 'Ka',
+//             marca: 'Ford', 
+//             placa: 'ABC-1234'
+//         }
+//     ]
+// }
+// console.log(concessionária.estoque[0].modelo)
+// console.log(concessionária['estoque'][1]['placa'])
+
+//Uma calculadora realiza as quatro operações fundamentais
+//soma: representada por uma arrow function que faz return
+//subtração: representada por uma arrow function sem return
+//multiplicação: function regular
+//divisão: escolha 
+
+// const calculadora = {
+//     operacoes:{
+//         soma:  (a, b) => {return a+b},
+//         subtracao: (a,b) => console.log(a-b), 
+//         multiplicacao: function (a, b){
+//             return a*b
+//         }, 
+//         divisao: (a,b) => {return a/b}
+//     }
+// }
+// console.log(calculadora.operacoes.soma(1,2))
+
+//Execução Síncrona (bloqueante) e Assíncrona
+// console.log('Eu primeiro')
+// console.log("Agora eu")
+// console.log("Sempre vou ser a última...:(")
+// const a = 5+6;
+// const b = 9+4;
+// console.log(a+b)
+// function demorada(tempo){
+//     const dataAtualMaisTempoSegundos = new Date().getTime() + tempo;      //data atual deslocada de 2s
+//     while(new Date().getTime() <= dataAtualMaisTempoSegundos);           //espera e não faz nada (espécie de espera ocupada)
+//     const d =8+2*6;
+//     console.log(`Demorada com tempo: ${tempo}`)
+//     return d
+// }
+
+// setTimeout(() => {
+//     demorada(5000)
+// }, 5000)
+
+// setTimeout(() => {
+//     demorada(1000)
+// }, 1000)           //entrou na fila após um segundo (antes).
+// console.log('Fim do script principal')
+// setTimeout(()=>{
+//     console.log('Agendada pelo setTimeout')         //jamais termina antes do script que fez o agendamento acabar
+// }, 0)
+// const dataAtualMais5Segundos = new Date().getTime() + 5000;
+// while(new Date().getTime() <= dataAtualMais5Segundos);
+// console.log('Terminando o script principal...')
+
+// const a = 2+3
+// const b = 6*1;
+
+// setTimeout(() => {
+//     const d = demorada()
+//     console.log(`d: ${d}`)
+// }, 500)   //agenda a execução de outra função. Passados 500ms, execute a função demorada() e exiba o valor de d
+
+// // const d = demorada()
+// const e = a+b*2
+// console.log(`e: ${e}`)
+
+//CPU Bound: prediminantemente caracterizada por ciclos de CPU
+//IO Bound: predominantemente caracterizada por operações de entrada e saída
+// const fs = require('fs')
+// const nomeArquivo = 'arquivo.txt'
+// //função callback: 
+// const exibirConteudo = (erro, conteudo) => {
+//     console.log('A')
+//     if(erro){
+//         console.log(`Erro: ${erro}`)
+//     }
+//     else{
+//         console.log(`Conteúdo: ${conteudo}`)
+//         const dobro = Number(conteudo.toString())*2;
+//         //função callback
+//         const finalizar = (erro) => {
+//             console.log(erro ? "Deu erro ao escrever o dobro" : "Ok, escreveu o dobro")
+//             console.log("C")
+//             const exibirDobro = (erro, conteudo) =>{
+//                 console.log("E")
+//                 console.log(erro ? "Deu erro lendo o dobro":`Dobro: ${conteudo.toString()}`)
+//             }
+//             fs.readFile('dobro.txt', exibirDobro)
+//             console.log("F")
+//         }
+//         fs.writeFile('dobro.txt', dobro.toString(), finalizar)
+//         console.log("D")
+//     }
+// }
+// fs.readFile(nomeArquivo, exibirConteudo)
+// console.log('B')
+
+//Promisses
+//1 + 2 + 3 + .... + n
+//demorando:
+const calculoDemorado = (n) => {
+    let cont=0;
+    for(let i=1; i<=n; i++)
+        cont +=i;
+    return cont
 }
-console.log(pessoa.endereco.rua)
-console.log(pessoa['endereco']['numero'])
+const res = calculoDemorado(1000)       //bloqueante
+console.log(res)
